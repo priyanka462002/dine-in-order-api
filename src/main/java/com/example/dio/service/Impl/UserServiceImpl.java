408abb9871baa072a5255df72577c2b94aa4ac03
priyanka462002 <lspriyanka04@gmail.com>
2025-02-27 17:08:11 +0530

    modified the UserServiceImpl

--- src/main/java/com/example/dio/service/Impl/UserServiceImpl.java
+++ src/main/java/com/example/dio/service/Impl/UserServiceImpl.java
@@ -1,5 +1,8 @@
 package com.example.dio.service.Impl;
 
+import com.example.dio.enums.UserRole;
+import com.example.dio.model.Admin;
+import com.example.dio.model.Staff;
 import com.example.dio.model.User;
 import com.example.dio.repository.UserRepository;
 import com.example.dio.service.UserService;
@@ -14,10 +17,28 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User registerUser(User user){
+        User user2 =this.createUserByRole(user.getRole());
 
-        return userRepository.save(user);
+        this.mapToNewUser(user,user2);
+        return userRepository.save(user2);
     }
 
+    private User createUserByRole(UserRole role){
+        User user;
+        switch (role){
+            case ADMIN -> user=new Admin();
+            case STAFF -> user=new Staff();
+            default -> throw new RuntimeException("Failed to register user,invalid user");
+        }
+        return user;
+    }
 
+ private void mapToNewUser(User user,User user2){
+        user2.setUsername(user.getUsername());
+        user2.setEmail(user.getEmail());
+        user2.setPassword(user.getPassword());
+        user2.setRole(user.getRole());
+        user2.setPhNo(user.getPhNo());
+ }
 
 }
