1fb163f5d4b499a051998c28e9d31bdfd882ecf5
priyanka462002 <lspriyanka04@gmail.com>
2025-02-27 16:48:21 +0530

    modified UserController

--- src/main/java/com/example/dio/controller/UserController.java
+++ src/main/java/com/example/dio/controller/UserController.java
@@ -2,6 +2,7 @@ package com.example.dio.controller;
 
 import com.example.dio.model.User;
 import com.example.dio.service.UserService;
+import com.example.dio.util.ResponseBuilder;
 import com.example.dio.util.ResponseStructure;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -17,15 +18,13 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/register")
-    public ResponseEntity<ResponseStructure<User>> registerUser(@RequestBody User user){
+    public ResponseEntity<ResponseStructure<String>> registerUser(@RequestBody User user){
         user =userService.registerUser(user);
 
-        ResponseStructure<User> structure = new ResponseStructure<>();
-        structure.setData(user);
-        structure.setStatus(HttpStatus.CREATED.value());
-        structure.setMessage("User Created");
 
-        return new ResponseEntity<ResponseStructure<User>>(structure,HttpStatus.CREATED);
+        return ResponseBuilder.success(HttpStatus.CREATED, "User Created", "user");
+
+
 
     }
 
