710f7dbc66cb6ad621f33bd6d72b3ff07e88859e
priyanka462002 <lspriyanka04@gmail.com>
2025-02-27 16:51:48 +0530

    modified ResponseStructure for scalability of project

--- src/main/java/com/example/dio/util/ResponseStructure.java
+++ src/main/java/com/example/dio/util/ResponseStructure.java
@@ -1,12 +1,24 @@
 package com.example.dio.util;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
+import org.springframework.http.HttpStatus;
 
 @Getter
-@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ResponseStructure<T> {
     private int status;
     private String message;
     private T data;
+
+    public static <T> ResponseStructure<T>create(HttpStatus status,String message,T data){
+        ResponseStructure<T> response =new ResponseStructure<>();
+
+        response.status=status.value();
+        response.message=message;
+        response.data=data;
+
+        return response;
+    }
 }
