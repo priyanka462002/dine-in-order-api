4d1cd24a0713cc8be3125fd639e9a9277c2788fb
priyanka462002 <lspriyanka04@gmail.com>
2025-03-03 21:50:09 +0530

    creation of ResponseBuilder class

--- src/main/java/com/example/dio/util/ResponseBuilder.java
+++ src/main/java/com/example/dio/util/ResponseBuilder.java
@@ -4,6 +4,8 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import java.util.List;
+
 public class ResponseBuilder {
 
     public static<T>ResponseEntity<ResponseStructure<T>> success(HttpStatus status,String message,T data){
@@ -33,6 +35,25 @@ public class ResponseBuilder {
                 .body(structure);
     }
 
+public static ResponseEntity<SimpleErrorResponse> error(HttpStatus status,String message){
+        SimpleErrorResponse error=SimpleErrorResponse.builder()
+                .type(status.name())
+                .message(message)
+                .status(status.value())
+                .build();
 
+        return ResponseEntity.status(status)
+                .body(error);
+
+}
+    public static ResponseEntity<FieldErrorResponse>error(HttpStatus status, String message, List<FieldErrorResponse.FieldError> errors){
+        FieldErrorResponse error =FieldErrorResponse.builder()
+                .type(status.name())
+                .status(status.value())
+                .message(message)
+                .build();
+        return  ResponseEntity.status(status)
+                .body(error);
+    }
 
 }
