bc093d55b766a9dd33f07d2596c6ac4c9cb512d5
priyanka462002 <lspriyanka04@gmail.com>
2025-03-04 17:17:18 +0530

    adding and changing the configuration of database that is removing the security details and placing it in a sepearte file called .env

--- src/main/java/com/example/dio/exception/handler/UserExceptionHandler.java
+++ src/main/java/com/example/dio/exception/handler/UserExceptionHandler.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class UserExceptionHandler {
 
     @ExceptionHandler(UserNotFoundByIdException.class)
-    public ResponseEntity<SimpleErrorResponse> getMessage(UserNotFoundByIdException ex){
+    public ResponseEntity<SimpleErrorResponse> handleUserNOtFoundByIdException(UserNotFoundByIdException ex){
         return ResponseBuilder.error(HttpStatus.NOT_FOUND,ex.getMessage());
     }
 }
