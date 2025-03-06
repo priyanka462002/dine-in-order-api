69fbc1d605d7f8ed9df9abd9476e875298317553
priyanka462002 <lspriyanka04@gmail.com>
2025-03-06 15:45:41 +0530

    adding the documentation

--- src/main/java/com/example/dio/mapper/UserMapper.java
+++ src/main/java/com/example/dio/mapper/UserMapper.java
@@ -10,9 +10,26 @@ import org.mapstruct.MappingTarget;
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
+    /**
+     *This method is typically used to convert an entity or domain model (User) to a response object (UserResponse) that can be sent as a response in an API.
+     * @param user  object that contains the data to be mapped.
+     * @return user response object containing the relevant data from the User object, often with sensitive information omitted.
+     */
     UserResponse mapToUserResponse(User user);
+    /**
+     *
+     * @param registrationRequest containing user registration details (e.g., username, email, password) to be mapped to the User entity.
+     * @param user entity that will be updated with the values from the registrationRequest.
+     *  method doesn't return anything, it directly modifies the User entity passed in as a parameter.
+     */
 
     void mapToUserEntity(RegistrationRequest registrationRequest, @MappingTarget User user);
+
+    /**
+     *
+     * @param userRequest containing the user data (e.g., username, email, etc.) to be mapped to the existing User entity.
+     * @param user entity that will be updated with the values from the userRequest. The @MappingTarget annotation indicates that this object will be modified by the mapping.
+     */
 
     void mapToUserEntity(UserRequest userRequest,@MappingTarget User user);
 
