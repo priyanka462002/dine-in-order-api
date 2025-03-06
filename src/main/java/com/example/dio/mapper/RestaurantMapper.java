18906d65a3b5bc0fd95a7c246e80a7ddfb6b0a00
priyanka462002 <lspriyanka04@gmail.com>
2025-03-06 23:19:57 +0530

    creation of RestaurantMapper

---
+++ src/main/java/com/example/dio/mapper/RestaurantMapper.java
@@ -0,0 +1,33 @@
+package com.example.dio.mapper;
+
+
+import com.example.dio.dto.request.RestaurantRequest;
+import com.example.dio.dto.response.RestaurantResponse;
+import com.example.dio.model.CuisineType;
+import com.example.dio.model.Restaurant;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface RestaurantMapper {
+
+    Restaurant mapToRestaurantEntity(RestaurantRequest restaurantRequest);
+
+    RestaurantResponse mapToRestaurantResponse(Restaurant restaurant);
+
+    default String mapToString(CuisineType cuisineType){
+        if(cuisineType == null){
+            return  null;
+        }
+        return cuisineType.getCuisineName();
+    }
+
+    default CuisineType mapToCuisineType(String cuisineType){
+        if(cuisineType == null){
+            return null;
+        }
+        CuisineType cuisine =new CuisineType();
+        cuisine.setCuisineName(cuisineType);
+        return cuisine;
+    }
+
+}
