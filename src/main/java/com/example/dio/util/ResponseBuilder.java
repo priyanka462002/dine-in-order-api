60da116a77d14741da058e67d90165d51376fe6c
priyanka462002 <lspriyanka04@gmail.com>
2025-03-06 15:49:08 +0530

    adding the documentation

--- src/main/java/com/example/dio/util/ResponseBuilder.java
+++ src/main/java/com/example/dio/util/ResponseBuilder.java
@@ -8,6 +8,16 @@ import java.util.List;
 
 public class ResponseBuilder {
 
+
+     /**
+     * Helps to create success response with data including the Http status code , message and data itself
+     * @param status it specifies the status code of the request
+     * @param message it specifies the success message of the request
+     * @param data  it specifies the data which is involved .
+     * @return it returns the data of type ResponseEntity<ResponseStructure<T>>
+     * */
+
+
     public static<T>ResponseEntity<ResponseStructure<T>> success(HttpStatus status,String message,T data){
         ResponseStructure<T> structure=ResponseStructure.<T>builder()
                 .status(status.value())
@@ -35,6 +45,13 @@ public class ResponseBuilder {
                 .body(structure);
     }
 
+    /**
+     * Helps to create error response including Httpstatus code,message itself.
+     * @param status returns the status type and  status cod of an error
+     * @param message returns the message of the errror
+     * @return it returns  the data of the type ResponseEntity<SimpleErrorResponse>>
+     **/
+
 public static ResponseEntity<SimpleErrorResponse> error(HttpStatus status,String message){
         SimpleErrorResponse error=SimpleErrorResponse.builder()
                 .type(status.name())
@@ -54,6 +71,40 @@ public static ResponseEntity<SimpleErrorResponse> error(HttpStatus status,String
                 .build();
         return  ResponseEntity.status(status)
                 .body(error);
+    }
+
+    /**
+     *Generates a successful HTTP response with a 200 OK status, wrapping the provided data and message in a ResponseStructure. This method simplifies creating standard responses for successful requests.
+     * @param data to be included in the response body. It can be of any type (T).
+     * @param message  to be included in the response, typically indicating the result or success of the operation.
+     * @return  A ResponseEntity containing a ResponseStructure object. The ResponseStructure holds the provided message and data, and the response has a 200 OK status.
+     */
+
+    public static<T> ResponseEntity<ResponseStructure<T>>ok(T data,String message){
+        return success(HttpStatus.OK,message,data);
+    }
+
+    /**
+     *Generates a successful HTTP response with a 200 OK status, wrapping the provided data and message in a ResponseStructure. This method simplifies creating standard responses for successful requests.
+     * @param data to be included in the response body. It can be of any type (T).
+     * @param message  to be included in the response, typically indicating the result or success of the operation.
+     * @return  A ResponseEntity containing a ResponseStructure object. The ResponseStructure holds the provided message and data, and the response has a 200 OK status.
+     */
+
+
+    public static<T> ResponseEntity<ResponseStructure<T>>created(T data,String message){
+        return success(HttpStatus.CREATED,message,data);
+
+    }
+
+    /**
+     *Generates an HTTP 404 Not Found response with a custom error message. This method is typically used to return an error when a requested resource is not found.
+     * @param message describing why the resource was not found.
+     * @return A ResponseEntity containing a SimpleErrorResponse object, which holds the provided error message. The response will have an HTTP status of 404 Not Found.
+     */
+
+    public static ResponseEntity<SimpleErrorResponse>notFound(String message){
+        return error(HttpStatus.NOT_FOUND,message);
     }
 
 }
