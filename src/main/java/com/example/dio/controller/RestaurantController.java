b20baccd6b60801c298ab96ecb456c3f13abeea9
priyanka462002 <lspriyanka04@gmail.com>
2025-03-11 16:23:51 +0530

    specifiying the endpoints

---
+++ src/main/java/com/example/dio/controller/RestaurantController.java
@@ -0,0 +1,47 @@
+package com.example.dio.controller;
+
+import com.example.dio.dto.request.RestaurantRequest;
+import com.example.dio.dto.response.RestaurantResponse;
+import com.example.dio.service.RestaurantService;
+import com.example.dio.util.FieldErrorResponse;
+import com.example.dio.util.ResponseBuilder;
+import com.example.dio.util.ResponseStructure;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+
+@RestController
+@AllArgsConstructor
+@Tag(name="RestaurantController", description = "Endpoints for managing restaurant data")
+public class RestaurantController {
+
+    private final RestaurantService restaurantService;
+
+    @PostMapping("/users/{userId}/restaurants")
+    @Operation(description = "Create a new restaurant if the user is an admin",
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "Restaurant Created"),
+                    @ApiResponse(responseCode = "400", description = "Invalid Input", content = {
+                            @Content(schema = @Schema(implementation = FieldErrorResponse.class))
+                    }),
+                    @ApiResponse(responseCode = "403", description = "User is not an admin", content = {
+                            @Content(schema = @Schema(implementation = FieldErrorResponse.class))
+                    })
+            })
+    public ResponseEntity<ResponseStructure<RestaurantResponse>> createRestaurant(@RequestBody @Valid RestaurantRequest restaurantRequest, @PathVariable long userId) {
+        RestaurantResponse response = restaurantService.createRestaurant(restaurantRequest,userId);
+        return ResponseBuilder.created(response, "Restaurant Created");
+    }
+
+
+}
+
+
