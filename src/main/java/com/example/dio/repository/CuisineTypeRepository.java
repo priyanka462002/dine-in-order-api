8a67aab609bfd4f9e84159ea9f238c95e1104baa
priyanka462002 <lspriyanka04@gmail.com>
2025-03-11 16:48:43 +0530

    CuisineTypeRepository creation

---
+++ src/main/java/com/example/dio/repository/CuisineTypeRepository.java
@@ -0,0 +1,10 @@
+package com.example.dio.repository;
+
+import com.example.dio.model.CuisineType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CuisineTypeRepository extends JpaRepository<CuisineType, String> {
+
+
+}
+
