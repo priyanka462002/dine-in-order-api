f95b6f4181316c81b68e464cc259c397c99be37a
priyanka462002 <lspriyanka04@gmail.com>
2025-03-12 23:13:27 +0530

    creation of the cartItemrResponse dto

---
+++ src/main/java/com/example/dio/dto/response/CartItemResponse.java
@@ -0,0 +1,13 @@
+package com.example.dio.dto.response;
+
+
+
+public class CartItemResponse {
+
+    private long cartId;
+    private int quantity;
+    private double totalPrice;
+    private boolean isOrdered;
+
+
+}
