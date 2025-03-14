f7d0c6dc6d7f518feabc52ccc3b5f84df289ef02
priyanka462002 <lspriyanka04@gmail.com>
2025-03-14 09:31:34 +0530

    modifying the CartItemResponse

--- src/main/java/com/example/dio/dto/response/CartItemResponse.java
+++ src/main/java/com/example/dio/dto/response/CartItemResponse.java
@@ -1,13 +1,21 @@
 package com.example.dio.dto.response;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class CartItemResponse {
 
-    private long cartId;
-    private int quantity;
-    private double totalPrice;
-    private boolean isOrdered;
+    private FoodItemResponse foodItemResponse;
 
+    private int quantity;
+
+    private double totalPrice;
 
 }
