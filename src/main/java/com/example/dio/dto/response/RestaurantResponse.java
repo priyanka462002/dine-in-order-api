5ed135a68fd12fda9a6d4e0cac0a5f3586ddf4c4
priyanka462002 <lspriyanka04@gmail.com>
2025-03-14 09:35:50 +0530

    modifying the RestaurantResponse

--- src/main/java/com/example/dio/dto/response/RestaurantResponse.java
+++ src/main/java/com/example/dio/dto/response/RestaurantResponse.java
@@ -1,6 +1,7 @@
 package com.example.dio.dto.response;
 
 import com.example.dio.enums.DietType;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,14 +11,24 @@ import java.util.List;
 
 @Getter
 @Setter
+@Builder
 public class RestaurantResponse {
+
     private long restaurantId;
+
     private String restaurantName;
+
     private String address;
+
     private String contactNumber;
+
     private String contactEmail;
+
     private LocalTime opensAt;
+
     private LocalTime closesAt;
+
     private List<DietType> dietTypes;
+
     private List<String>cuisineTypes;
 }
