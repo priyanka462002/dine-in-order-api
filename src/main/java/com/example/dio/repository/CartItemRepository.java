3999462908c98833a84181f305fe96090fc96a54
priyanka462002 <lspriyanka04@gmail.com>
2025-03-14 09:46:57 +0530

    added the methods

--- src/main/java/com/example/dio/repository/CartItemRepository.java
+++ src/main/java/com/example/dio/repository/CartItemRepository.java
@@ -1,8 +1,25 @@
 package com.example.dio.repository;
 
 import com.example.dio.model.CartItem;
+import com.example.dio.model.RestaurantTable;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
 
 public interface CartItemRepository extends JpaRepository<CartItem,Long> {
+
+
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE CartItem c SET c.quantity = :newQuantity WHERE c.id = :cartItemId")
+    int updateQuantityByCartItemId(Long cartItemId, int newQuantity);
+
+    List<CartItem> findByIsOrderedAndRestaurantTable_TableId(Boolean False,Long tableId) ;
 
 }
